652515ed74b69d1fd664ee1a1aa90ba8318a16cb
gokulrajalp <gokulrajalp@gmail.com>
2022-05-01 20:15:56 +0530

    Updates

---
+++ precache-manifest.e2f4373e0fd0991c5e13225fa51cfce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2764346bd6a3b2acb004",
+    "url": "/DeptCoffer/static/css/main.7bbb7505.chunk.css"
+  },
+  {
+    "revision": "2764346bd6a3b2acb004",
+    "url": "/DeptCoffer/static/js/main.2764346b.chunk.js"
+  },
+  {
+    "revision": "a269d63d017438b046c2",
+    "url": "/DeptCoffer/static/js/1.a269d63d.chunk.js"
+  },
+  {
+    "revision": "a088a9b4011e682cb1ce",
+    "url": "/DeptCoffer/static/js/runtime~main.a088a9b4.js"
+  },
+  {
+    "revision": "f7865577aa9c282568687a59f0add77c",
+    "url": "/DeptCoffer/index.html"
+  }
+];
